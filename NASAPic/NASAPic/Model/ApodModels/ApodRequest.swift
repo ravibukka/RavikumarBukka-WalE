0648942d0b13f90b3ff623227a744cf9b9cc1d43
Ravikumar Bukka <>
2022-04-21 23:05:09 +0530

    adding model classes

---
+++ NASAPic/NASAPic/Model/ApodModels/ApodRequest.swift
@@ -0,0 +1,10 @@
+//
+//  ApodRequest.swift
+//  NASAPic
+//
+//  Created by Administrator on 21/04/22.
+//
+
+struct ApodRequest : Encodable {
+    var date: String
+}
---
+++ NASAPic/NASAPic/Model/ApodModels/ApodResponse.swift
@@ -0,0 +1,23 @@
+//
+//  ApodResponse.swift
+//  NASAPic
+//
+//  Created by Administrator on 21/04/22.
+//
+
+class ApodResponse : Decodable {
+    var date: String
+    var title: String
+    var explanation: String
+    var url: String
+    var media_type: String
+    
+    init(date:String,title: String,explanation: String,url: String,media_type: String) {
+        
+        self.date = date
+        self.title = title
+        self.explanation = explanation
+        self.url = url
+        self.media_type = media_type
+    }
+}
