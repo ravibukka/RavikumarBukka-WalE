c7440967eed06f6435c0acbe4c967938db4e7a25
Ravikumar Bukka <>
2022-04-21 22:48:53 +0530

    Adjusting folder structure and adding view and storyboard and common or constant files

---
+++ NASAPic/NASAPic/Common/Common.swift
@@ -0,0 +1,55 @@
+//
+//  Common.swift
+//  NASAPic
+//
+//  Created by Administrator on 21/04/22.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let ErrorAlertTitle = "Error"
+    static let OkAlertTitle = "Ok"
+    static let CancelAlertTitle = "Cancel"
+    static let CalenderImage = "calender"
+    static let NoImage = "no-image-icon"
+    static let ApodDateFormat = "yyyy-MM-dd"
+    static let MediaTypeImage = "image"
+    static let MediaTypeVideo = "video"
+    static let CachedResponseKey = "CachedResponse"
+    enum MediaType {
+        case image
+        case video
+    }
+    
+    static func getAPIKey() -> String {
+        
+        let config = getConfig()
+        return config?["APOD_KEY"] as? String ?? ""
+    }
+    
+    static func getApodBaseURL() -> String {
+        
+        let config = getConfig()
+        return config?["BASE_URL"] as? String ?? ""
+    }
+    
+    static func getConfig() -> [String: Any]? {
+        
+        var config: [String: Any]?
+        
+        if let infoPlistPath = Bundle.main.url(forResource: "Info", withExtension: "plist") {
+            do {
+                let infoPlistData = try Data(contentsOf: infoPlistPath)
+                if let dict = try PropertyListSerialization.propertyList(from: infoPlistData, options: [], format: nil) as? [String: Any] {
+                    config = dict
+                }
+            } catch {
+                print(error)
+            }
+        }
+        return config
+    }
+}
+
